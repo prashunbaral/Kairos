6a6a152b8acf60ee5b7b83d3f7d6273b4a56fcd1
prashun <prashunbaral@gmail.com>
2024-06-18 12:20:37 +0545

    handled deplyment error

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -11,39 +11,39 @@ const Page = () => {
 
   const sessionId = searchParams.get("session_id");
 
-    const confirmOrderCreate = async () => {
-      try {
-        const response = await axios.post("/api/stripe-sessions", {
-          sessionId,
-        });
-        // const data = await response.json(
-        console.log(response);
-        toast.success(response.data)
-        console.log("Response", response.data);
-      } catch (error) {
-        setIsError(true);
-        toast.error("Error storing data in database")
-        console.log("Axios Error", error);
-      }
-    };
-    confirmOrderCreate();
+  const confirmOrderCreate = async () => {
+    try {
+      const response = await axios.post("/api/stripe-sessions", {
+        sessionId,
+      });
+      // const data = await response.json(
+      console.log(response);
+      toast.success(response.data);
+      console.log("Response", response.data);
+    } catch (error) {
+      setIsError(true);
+      toast.error("Error storing data in database");
+      console.log("Axios Error", error);
+    }
+  };
+  confirmOrderCreate();
 
   return (
     <Suspense fallback={<div>Loading ...</div>}>
-    <div>
-      {isError ? (
-        <div className="h-screen w-full flex items-center justify-center">
-          <p className="text-3xl text-red-500">
-            Error Storing the order in database. Contact the developer
-          </p>
-        </div>
-      ) : (
-        <div className="h-screen w-full flex items-center justify-center flex-col">
-          <p className="text-3xl text-green-500">Payment Successful. </p>
-          <Link href={"/"}>Go to HomePage</Link>
-        </div>
-      )}
-    </div>
+      <div>
+        {isError ? (
+          <div className="h-screen w-full flex items-center justify-center">
+            <p className="text-3xl text-red-500">
+              Error Storing the order in database. Contact the developer
+            </p>
+          </div>
+        ) : (
+          <div className="h-screen w-full flex items-center justify-center flex-col">
+            <p className="text-3xl text-green-500">Payment Successful. </p>
+            <Link href={"/"}>Go to HomePage</Link>
+          </div>
+        )}
+      </div>
     </Suspense>
   );
 };
